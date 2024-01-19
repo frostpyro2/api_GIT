4acd6b3d3dc821812a2310f6074c4bd379924e0f
frostpyro2 <143861009+frostpyro2@users.noreply.github.com>
2024-01-19 17:48:46 +0900

    modifying thread method

--- src/main/java/frostpyro/frostapi/threads/SkillThread.java
+++ src/main/java/frostpyro/frostapi/threads/SkillThread.java
@@ -1,6 +1,14 @@
 package frostpyro.frostapi.threads;
 
+import org.bukkit.entity.Player;
+
 public class SkillThread implements Runnable{
+    private Player player;
+
+    public SkillThread(Player player){
+        this.player = player;
+    }
+
     @Override
     public void run() {
 
