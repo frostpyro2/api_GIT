94e0b9df4f3f6f6fb7be9fbec1c6c78655ffa6fe
frostpyro <park35121454@gmail.com>
2024-01-22 17:48:53 +0900

    .

---
+++ src/main/java/frostpyro/frostapi/graphic_user_interface/EXP_GUI/ExpGUI.java
@@ -0,0 +1,17 @@
+package frostpyro.frostapi.graphic_user_interface.EXP_GUI;
+
+import frostpyro.frostapi.graphic_user_interface.User_Interface;
+
+public class ExpGUI implements User_Interface {
+    //TODO: get max_val and player's val from SQL or saved data in yml
+    //make GUI by boss health bar
+    @Override
+    public void guiType() {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+}
--- src/main/java/frostpyro/frostapi/graphic_user_interface/select_GUI/InventoryGUI.java
+++ src/main/java/frostpyro/frostapi/graphic_user_interface/select_GUI/InventoryGUI.java
@@ -3,6 +3,7 @@ package frostpyro.frostapi.graphic_user_interface.select_GUI;
 import frostpyro.frostapi.graphic_user_interface.User_Interface;
 
 public class InventoryGUI implements User_Interface{
+    //selecting skill from inventory GUI
     @Override
     public void show() {
 
