695404fd1ced85b7bd29ebd9abefc199c9dae8eb
frostpyro <143861009+frostpyro2@users.noreply.github.com>
2024-01-23 12:48:19 +0900

    .

--- src/main/java/frostpyro/frostapi/graphic_user_interface/EXP_GUI/ExpGUI.java
+++ src/main/java/frostpyro/frostapi/graphic_user_interface/EXP_GUI/ExpGUI.java
@@ -1,10 +1,14 @@
 package frostpyro.frostapi.graphic_user_interface.EXP_GUI;
 
+import frostpyro.frostapi.FrostAPI;
 import frostpyro.frostapi.graphic_user_interface.User_Interface;
+import org.bukkit.Bukkit;
+import org.bukkit.boss.BossBar;
 
 public class ExpGUI implements User_Interface {
     //TODO: get max_val and player's val from SQL or saved data in yml
     //make GUI by boss health bar
+    private BossBar expBar = Bukkit.getBossBar(FrostAPI.getPlugin().exp);
     @Override
     public void guiType() {
 
