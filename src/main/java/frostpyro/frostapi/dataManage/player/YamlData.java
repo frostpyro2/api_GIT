0a018b158a8f447b970ab926e54974a11ab90891
frostpyro <143861009+frostpyro2@users.noreply.github.com>
2024-02-17 16:34:43 +0900

    .

--- src/main/java/frostpyro/frostapi/dataManage/player/YamlData.java
+++ src/main/java/frostpyro/frostapi/dataManage/player/YamlData.java
@@ -5,20 +5,20 @@ import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
 
 import java.io.File;
 import java.io.IOException;
 
 public class YamlData implements DataManage{
     @Override
-    public PlayerData getPlayerData(Player player) {
+    public @Nullable PlayerData getPlayerData(Player player) {
         File playerFile = new File(FrostAPI.getPlugin().getDataFolder(), "player\\"+player.getName()+"_"+ player.getUniqueId().toString()+".yml");
         FileConfiguration configuration = new YamlConfiguration();
         try{
             configuration.load(playerFile);
         }
         catch (IOException | InvalidConfigurationException e){
-            e.printStackTrace();
             return null;
         }
         return new PlayerData(player.getUniqueId().toString(), player.getName(), configuration.getInt("skillID"), configuration.getInt("level"), configuration.getDouble("exp"), configuration.getDouble("money"));
