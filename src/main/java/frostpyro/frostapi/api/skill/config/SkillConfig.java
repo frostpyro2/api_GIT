20be53cda7d5b899e06b620eabcf46205301a77e
frostpyro <143861009+frostpyro2@users.noreply.github.com>
2024-02-23 23:32:52 +0900

    .

--- src/main/java/frostpyro/frostapi/api/skill/config/SkillConfig.java
+++ src/main/java/frostpyro/frostapi/api/skill/config/SkillConfig.java
@@ -2,7 +2,14 @@ package frostpyro.frostapi.api.skill.config;
 
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.File;
+import java.io.IOException;
 
 public class SkillConfig extends YamlConfiguration {
-
+    private File file;
+    public SkillConfig(File file) {
+        this.file = file;
+    }
 }
