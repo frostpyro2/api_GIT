240c1f811a8323c836f5be270d9e857d09b81620
frostpyro <143861009+frostpyro2@users.noreply.github.com>
2024-02-23 23:53:20 +0900

    .

---
+++ src/main/java/frostpyro/frostapi/api/skill/dir/SkillYmlDir.java
@@ -0,0 +1,21 @@
+package frostpyro.frostapi.api.skill.dir;
+
+public enum SkillYmlDir {
+    DAMAGE("damage"),
+    PARTICLE("particle"),
+    COOL_DOWN("coolDown"),
+    PROJECTILE("projectile"),
+    HOMING("homing"),
+    DISPLAY("display"),
+    ITEM("item"),
+    MODEL("model")
+    ;
+    private String string;
+    SkillYmlDir(String string){
+        this.string = string;
+    }
+
+    public String getDir(){
+        return string;
+    }
+}
